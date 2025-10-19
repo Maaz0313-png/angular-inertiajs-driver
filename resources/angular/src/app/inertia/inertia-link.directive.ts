f8fca33b6930782ffc2b02fb340e7836c5c819ea
Maaz Tajammul <maaztajammul63@gmail.com>
2025-10-20 02:13:35 +0500

    Enhance InertiaLinkDirective with click event handling

--- resources/angular/src/app/inertia/inertia-link.directive.ts
+++ resources/angular/src/app/inertia/inertia-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, HostListener, Input, ElementRef } from '@angular/core';
 import { router } from "@inertiajs/core";
 
 @Directive({
@@ -8,7 +8,29 @@ export class InertiaLinkDirective {
 
   @Input('inertiaLink') href = '';
 
-  @HostListener('click') onClick() {
-    router.get(this.href);
+  constructor(private el: ElementRef<HTMLElement>) {}
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    // Only handle primary (usually left) button
+    if (event.button !== 0) {
+      return;
+    }
+
+    // Respect modifier keys so users can open in new tab/window
+    if (event.metaKey || event.ctrlKey || event.shiftKey || event.altKey) {
+      return;
+    }
+
+    // If no explicit href input, try to read from host element (e.g. <a href="...">)
+    const href = this.href || (this.el.nativeElement.getAttribute && this.el.nativeElement.getAttribute('href')) || '';
+
+    if (!href) {
+      return;
+    }
+
+    // Prevent native navigation and use Inertia router
+    event.preventDefault();
+    router.get(href);
   }
 }
